ca43573bdb8f5c219a34c010a1e27c4874a7a71f
Bhuvi05 <37832087+Bhuvi05@users.noreply.github.com>
2018-03-27 16:17:37 +0530

    bhu5

---
+++ bhu5.py
@@ -0,0 +1,8 @@
+b = input("The given Input a letter of the alphabet: ")
+
+if b in ('a', 'e', 'i', 'o', 'u'):
+	print("%s\nthe given alphabet is a vowel." % b)
+
+else:
+	print("%sthe given alphabet is not a vowel." % b) 
+	
